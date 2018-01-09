4c250a714e61f410fd1249c09f5d25510c7bf459
Luigi Calori <luigi.calori@gmail.com>
2018-01-09 06:36:14 +0100

    first step for osx sierra spack deployment

---
+++ config/rcm/client/darwin/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+RCM_DEPLOY_CURRENT_PATH=deploy/rcm_client/dev/spack
+
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symli
+nk
+  DIR="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symli
+nk, we need to resolve it relative to the path where the symlink file was locate
+d
+done
+export RCM_DEPLOY_HOSTPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+source ${RCM_DEPLOY_HOSTPATH}/../../../../get_root
+
+
+python ${RCM_DEPLOY_ROOTPATH}/scripts/config.py -c config config/shared_install config/rcm config/rcm/client config/rcm/client/darwin  --platformconfig --runconfig --dest ${RCM_DEPLOY_CURRENT_PATH=}
+source ${RCM_DEPLOY_ROOTPATH}/${RCM_DEPLOY_CURRENT_PATH}/share/spack/setup-env.sh
+
