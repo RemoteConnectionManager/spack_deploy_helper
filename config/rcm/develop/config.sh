8cd614ffe249b5a0ad67a1b62f52fdc4909311f2
Luigi Calori <luigi.calori@gmail.com>
2018-06-01 10:54:12 +0200

    move heavvy install in install step

--- config/rcm/develop/config.sh
+++ config/rcm/develop/config.sh
@@ -1,4 +1,4 @@
-spack install --only dependencies rcm@develop
+#spack install --only dependencies rcm@develop
 #spack uninstall -y rcm@develop
-spack diy --source-path @{RCM_DEPLOY_ROOTPATH}/../RCM rcm@develop
+#spack diy --source-path @{RCM_DEPLOY_ROOTPATH}/../RCM rcm@develop
 
---
+++ config/rcm/develop/install.sh
@@ -0,0 +1,4 @@
+spack install -v --only dependencies rcm@develop
+#spack uninstall -y rcm@develop
+spack diy --source-path @{RCM_DEPLOY_ROOTPATH}/../RCM rcm@develop
+
