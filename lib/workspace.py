4c0751a4e3046333fee4c244bd1ae9514713d929
Luigi Calori <luigi.calori@gmail.com>
2018-11-16 19:56:58 +0100

    first cut of git_deploy function

--- lib/workspace.py
+++ lib/workspace.py
@@ -1,6 +1,24 @@
 import os
+import sys
 import uuid
+import logging
 
+root_path = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+if not root_path in sys.path:
+    sys.path.append(os.path.join(root_path, 'lib'))
+
+import utils
+
+rootLogger = logging.getLogger()
+rootLogger.setLevel(logging.INFO)
+consoleHandler = logging.StreamHandler()
+rootLogger.addHandler(consoleHandler)
+
+mylogger = logging.getLogger(__name__)
+
+#ls=utils.log_setup()
+logging.info("__file__:" + os.path.realpath(__file__))
+#ls.set_args()
 
 class WorkspaceManager():
     def __init__(self, path):
@@ -25,3 +43,84 @@ class WorkspaceManager():
             os.rmdir(path)
         except OSError:
             print('error: failed to remove the directory ' + path)
+
+    def git_deploy(self,
+                   dry_run=False,
+                   do_update=False,
+                   integration=False,
+                   branches=['clean/develop'],
+                   prlist=[],
+                   origin='https://github.com/RemoteConnectionManager/spack.git',
+                   origin_master='develop',
+                   pull_flags=['ff-only'],
+                   upstream='https://github.com/LLNL/spack.git'):
+
+
+        dest=os.path.join(self.base_path, 'dummy_spack_folder')
+
+        dev_git = utils.git_repo(dest, logger=mylogger, dry_run=dry_run)
+
+        if not os.path.exists(dest):
+            mylogger.info("MISSING destintion_dir-->" + dest + "<-- ")
+            os.makedirs(dest)
+
+            origin_branches = utils.get_branches(origin, branch_selection=branches)
+
+            dev_git.init()
+
+            dev_git.get_remotes()
+            dev_git.add_remote(origin, name='origin', fetch_branches=origin_branches)
+            dev_git.add_remote(upstream, name='upstream')
+
+            upstream_branches = utils.get_branches(
+                upstream,
+                branch_pattern='.*?\s+refs/pull/([0-9]*?)/head\s+',
+                # branch_exclude_pattern='.*?\s+refs/pull/({branch})/merge\s+',
+                branch_format_string='pull/{branch}/head',
+                branch_selection=prlist)
+
+            local_pr = utils.trasf_match(upstream_branches, in_match='.*/([0-9]*)/(.*)', out_format='pull/{name}/clean')
+
+            mylogger.info("upstream_branches->" + str(upstream_branches) + "<--")
+
+            dev_git.fetch(name='origin', branches=origin_branches)
+
+            if integration:
+                if len(origin_branches) > 0:
+                    upstream_clean = origin_branches[0]
+                    print("--------------------------------------" + upstream_clean + "-----------------------")
+                    dev_git.checkout(upstream_clean)
+                    dev_git.sync_upstream()
+                    dev_git.checkout(upstream_clean, newbranch=origin_master)
+
+                    dev_git.sync_upstream()
+
+                    for b in origin_branches[1:]:
+                        dev_git.checkout(b)
+                        dev_git.sync_upstream(options=['--rebase'])
+
+                    dev_git.fetch(name='upstream', branches=local_pr)
+
+                    for n, branch in local_pr.items():
+                        mylogger.info("local_pr " + n + " " + branch)
+                        dev_git.checkout(branch, newbranch=branch + '_update')
+                        dev_git.merge(upstream_clean, comment='sync with upstream develop ')
+                        dev_git.checkout(origin_master)
+                        dev_git.merge(branch + '_update', comment='merged ' + branch)
+
+                    for branch in origin_branches[1:]:
+                        dev_git.checkout(origin_master)
+                        dev_git.merge(branch, comment='merged ' + branch)
+            else:
+                dev_git.fetch(name='origin', branches=[origin_master])
+                dev_git.checkout(origin_master)
+
+        else:
+            mylogger.info("Folder ->" + dest + "<- already existing, skipping git stuff")
+            if do_update:
+                mylogger.info("Updating Folder ->" + dest + "<-")
+                pull_options = []
+                for flag in pull_flags: pull_options.append('--' + flag)
+                for b in dev_git.get_local_branches():
+                    dev_git.checkout(b)
+                    dev_git.sync_upstream(upstream='origin', master=b, options=pull_options)
