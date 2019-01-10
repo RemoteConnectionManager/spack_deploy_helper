e98abf5338dd7cdc7383bb540cfde64ced1ada2c
Luigi Calori <luigi.calori@gmail.com>
2019-01-10 14:33:27 +0100

    add short prints for executed commands

--- lib/utils/run.py
+++ lib/utils/run.py
@@ -10,6 +10,7 @@ def run(cmd,logger=None,stop_on_error=True,dry_run=False,folder='.'):
         logger.warning("skipping empty command")
         return (0, '','')
     logger.info("running-->"+' '.join(cmd)+"<-\n"+os.environ['PATH'])
+    print("running-->"+' '.join(cmd))
     if not dry_run :
         myprocess = subprocess.Popen(cmd, cwd=folder,stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=os.environ)
         stdout,stderr = myprocess.communicate()
