3adc427768eb520cdc8391008247defe63d25f45
Luigi Calori <luigi.calori@gmail.com>
2019-01-17 10:35:21 +0100

    first version of interacive output from running commands

--- lib/utils/run.py
+++ lib/utils/run.py
@@ -3,25 +3,67 @@ import sys
 import subprocess
 import logging
 import json
+import fcntl
 
-def run(cmd,logger=None,stop_on_error=True,dry_run=False,folder='.'):
+def run(cmd,logger=None,
+        stop_on_error=True,
+        dry_run=False,
+        folder='.',
+        pipe_output=False):
     logger = logger or logging.getLogger(__name__)
     if not cmd :
         logger.warning("skipping empty command")
         return (0, '','')
-    logger.info("running-->"+' '.join(cmd)+"<-\n"+os.environ['PATH'])
-    print("running-->"+' '.join(cmd))
+    logger.info("running: "+' '.join(cmd))
+    # logger.debug("PATH: " + os.environ['PATH'])
+    # print("running-->"+' '.join(cmd))
     if not dry_run :
         myprocess = subprocess.Popen(cmd, cwd=folder,stdout=subprocess.PIPE,stderr=subprocess.PIPE, env=os.environ)
-        stdout,stderr = myprocess.communicate()
-        myprocess.wait()
-        ret = myprocess.returncode
+        if pipe_output:
+            #for f in [myprocess.stdout, myprocess.stderr]:
+            #    fd = f.fileno()
+            #    fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+            #    fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+            out_buf=''
+            err_buf=''
+            loop_var = True
+            ret=0
+            while loop_var:
+                #print("in while")
+                try:
+                    next_out_line = myprocess.stdout.readline()
+                    if next_out_line != '':
+                        out_buf += next_out_line
+                        logger.info(next_out_line.rstrip())
+                except:
+                    print("missing out")
+                    #sys.sleep(1)
+                    next_out_line=''
+                try:
+                    next_err_line = myprocess.stderr.readline()
+                    if next_err_line != '':
+                        err_buf += next_err_line
+                        logger.warning(next_err_line.rstrip())
+                    loop_var=True
+                except:
+                    print("missing err")
+                    next_err_line=''
+
+                #ret = myprocess.returncode
+                loop_var = next_err_line != '' or next_out_line != '' or myprocess.poll() is None
+            print("exited while")
+
+        else:
+            out_buf,err_buf = myprocess.communicate()
+            myprocess.wait()
+            ret = myprocess.returncode
         if ret:
             #print("ERROR:",ret,"Exiting")
             logger.error("ERROR CODE : " + str(ret) + '\n'+stderr+'\nExit...\n')
             if stop_on_error :
                 sys.exit()
-        return (ret,stdout,stderr)
+        # print("############################",stdout)
+        return (ret,out_buf,err_buf)
 
     else:
         logger.info("DRY RUN... nothing done")
