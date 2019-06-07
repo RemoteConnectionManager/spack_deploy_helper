74ec7d685d96e52627e3f651dfd2ba2d672e030f
Luigi Calori <luigi.calori@gmail.com>
2019-06-07 15:56:46 +0200

    install rcm dependencies

--- config/rcm/config.sh
+++ config/rcm/config.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 spack repo add @{RCM_DEPLOY_ROOTPATH}/repo --scope site
 spack info rcm
-#spack install --only dependencies rcm@develop
+spack install --only dependencies rcm@develop
 #spack uninstall -y rcm@develop
 #spack diy --source-path @{RCM_DEPLOY_ROOTPATH}/../RCM rcm@develop
 
