842d6b816e7c8e7e48deafa83d68ae977bb9b06e
Luigi Calori <luigi.calori@gmail.com>
2019-09-06 01:16:41 +0200

    build on Galileo

--- config/spack/devel/config.sh
+++ config/spack/devel/config.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
+spack spec -I curl
 spack install curl
+spack spec -I nasm
 spack install nasm
+spack spec -I cmake
 spack install cmake
 spack install --verbose py-flake8
 spack install perl
