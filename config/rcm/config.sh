fd1a7e3476fd840fa2bf178ff4d6efdc76f8d116
Luigi Calori <luigi.calori@gmail.com>
2019-09-19 17:02:45 +0200

    add openbox xstartup

--- config/rcm/config.sh
+++ config/rcm/config.sh
@@ -2,7 +2,7 @@
 spack repo add @{RCM_DEPLOY_ROOTPATH}/repo --scope site
 spack info rcm
 spack install --only dependencies rcm
-spack install rcm@dev
+spack install rcm@dev+linksource
 #spack uninstall -y rcm@develop
 #spack diy --source-path @{RCM_DEPLOY_ROOTPATH}/../RCM rcm@develop
 
