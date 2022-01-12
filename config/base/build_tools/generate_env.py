29bb447e04196dbc69669d4ff5b5613348e20d3c
Luigi Calori <l.calori@cineca.it>
2022-01-12 12:32:28 +0100

    correct typo in print exceptions

--- config/base/build_tools/generate_env.py
+++ config/base/build_tools/generate_env.py
@@ -5,16 +5,16 @@ substitutions={'COMPILER_SPEC' : 'gcc', 'OPENSSL_SPEC' : 'openssl'}
 try:
     substitutions['COMPILER_SPEC'] =  str(sorted([s['compiler']['spec'] for s in spack.config.get('compilers') if ( '/usr' in s['compiler']['paths']['cc'] ) and (s['compiler']['spec'].split('@')[0] == substitutions['COMPILER_SPEC'] )])[0])
 except Exception as exception :
-    print("unable to set compiler spec for exeception: " + str(esception) + " using " + substitutions['COMPILER_SPEC'] + " for COMPILER_SPEC")
+    print("unable to set compiler spec for exeception: " + str(exception) + " using " + substitutions['COMPILER_SPEC'] + " for COMPILER_SPEC")
 #from config       'SYS_OPENSSL_VER' : spack.config.get('packages')['openssl']['externals'][0]['spec'].split('@')[1]}
 try:
     substitutions['OPENSSL_SPEC'] = substitutions['OPENSSL_SPEC'] + '@' + str(sorted([s.version for s in spack.store.db.query(substitutions['OPENSSL_SPEC']) if s.external])[0])
 except Exception as exception :
-    print("unable to extract OPENSSL_SPEC from external spack.store.db.query due to exeception: " + str(esception))
+    print("unable to extract OPENSSL_SPEC from external spack.store.db.query due to exeception: " + str(exception))
     try:
         substitutions['OPENSSL_SPEC'] = str(sorted([ext['spec'] for ext in spack.config.get('packages')[substitutions['OPENSSL_SPEC']]['externals'] if '/usr' in ext['prefix']])[0])
     except Exception as exception :
-        print("unable to extract OPENSSL_SPEC from external in config packages due to exeception: " + str(esception))
+        print("unable to extract OPENSSL_SPEC from external in config packages due to exeception: " + str(exception))
 with open(sys.argv[1], mode='r', encoding='utf-8')  as filein:
     with open(sys.argv[2], mode='w', encoding='utf-8')  as fileout:
         for line in filein :
