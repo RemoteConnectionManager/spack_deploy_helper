d6b747d26e427539b1e03a1bbda991e6677b693e
Luigi Calori <l.calori@cineca.it>
2022-01-25 17:16:34 +0100

    some fixes for add external

--- scripts/add_external.py
+++ scripts/add_external.py
@@ -6,8 +6,9 @@ if len(sys.argv) < 3:
 out_compiler_spec=''
 if len(sys.argv) >= 4:
     out_compiler_spec='%'+sys.argv[3]
-
-matching_specs=sorted([s for s in spack.store.db.query(sys.argv[1]) if not s.external], key=lambda spc: spc.version)
+package_available_versions = spack.repo.get(sys.argv[1]).versions.keys()
+installed_specs = spack.store.db.query(sys.argv[1])
+matching_specs=sorted([s for s in installed_specs if not s.external and s.version in package_available_versions], reverse=True, key=lambda spc: spc.version)
 if len(matching_specs) == 0:
     print("no matching installed spec to " + sys.argv[1])
     exit(1)
@@ -16,16 +17,25 @@ if len(matching_specs) > 1:
     print(str(matching_specs))
     print("continue taking first")
 matching_spec=matching_specs[0]
+
+substitutions={
+        'EXTERNAL_NAME' : str(matching_spec.name),
+        'EXTERNAL_PREFIX' : str(matching_spec.prefix),
+}
+exclude_variants = ['build_type']
+new_spec += ''.join([str(vitem[1]) for vitem in curr_spec.variants.items() if not vitem[0] in exclude_variants])
+substitutions['NEW_SPEC'] =  str(matching_spec.name) + '@' + 
+                             str(matching_spec.version) + 
+                             out_compiler_spec + 
+                             ''.join([str(vitem[1]) for vitem in curr_spec.variants.items() if not vitem[0] in exclude_variants])
+
 template_string="""
 packages:
   ${EXTERNAL_NAME}:
     externals:
     - spec: ${NEW_SPEC}
       prefix: ${EXTERNAL_PREFIX}
+      buildable: false
 """
-substitutions={
-        'EXTERNAL_NAME' : str(matching_spec.name),
-        'EXTERNAL_PREFIX' : str(matching_spec.prefix),
-        'NEW_SPEC' : str(matching_spec.name) + '@' + str(matching_spec.version) + out_compiler_spec}
 with open(sys.argv[2], mode='w', encoding='utf-8')  as fileout:
     fileout.write(string.Template( template_string).safe_substitute(substitutions))
