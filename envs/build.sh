ec03dfcaa7a4657407b1ab0196a4bc0c63957f1d
Luigi Calori <l.calori@cineca.it>
2022-02-08 02:23:00 +0100

    rewrite and simplify env build template by uising euristics of SPACK_USER_CACHE_PATH and use lists inside spack.yaml templates

--- envs/build.sh
+++ envs/build.sh
@@ -11,39 +11,25 @@ done
 CURRPATH="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
 ROOTPATH="$(dirname $CURRPATH)"
 if [ "x$1" == "x" ]; then
-  echo " too few par, required tmpldir outdir compiler"
+  echo " too few par, required tmpldir compiler"
   exit 1 
 else
   TMPLDIR=$1
-  echo "template dir is: ${OUTDIR}"
+  echo "template dir is: ${TMPLDIR}"
   shift
 fi
 if [ "x$1" == "x" ]; then
-  echo " too few par, required tmpldir outdir compiler"
-  exit 1 
-else
-  OUTDIR=$1
-  echo "output_dir is: ${OUTDIR}"
-  shift
-fi
-if [ "x$1" == "x" ]; then
-  echo " too few par, required outdir compiler"
+  echo " too few par, required tmpldir compiler"
   exit 1 
 else
   COMPILER=$1
   shift
   echo "compiler is: ${COMPILER}"
 fi
-ENVPATH=${OUTDIR}/$(basename $TMPLDIR)_${COMPILER}
+ENVPATH=$(dirname $(spack-python ${ROOTPATH}/scripts/select_spec.py --loglevel debug --compiler ${COMPILER}  --outfile ${TMPLDIR}_${COMPILER}/spack.yaml --tplfile ${CURRPATH}/${TMPLDIR}/spack.yaml))
 echo "creating environment in: ${ENVPATH}"
-
-COMPILER_SPEC=$(spack-python ${ROOTPATH}/scripts/select_spec.py -t '${COMPILER}' -c ${COMPILER})
-echo "using compiler spec: ${COMPILER_SPEC}"
-echo "adding libraries: $@"
-spack env create --dir ${ENVPATH} ${CURRPATH}/${TMPLDIR}/spack.yaml 
+spack env deactivate
 spack env activate ${ENVPATH} 
-spack add -l compilers "%${COMPILER_SPEC}"
-spack add -l compiler_libs "$@"
 spack concretize -f 
 spack fetch --deprecated 
 spack env deactivate
