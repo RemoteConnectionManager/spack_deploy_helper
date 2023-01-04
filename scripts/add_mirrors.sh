501ea9827145cb0733225e0de04d64c1765ff3ab
Luigi Calori <l.calori@cineca.it>
2023-01-04 21:56:56 +0100

    increase folder dep search for caches in sibling folders

--- scripts/add_mirrors.sh
+++ scripts/add_mirrors.sh
@@ -2,7 +2,7 @@
 currpath=$(realpath $1)
 rootpath=$(realpath $currpath/../..)
  
-for p in $(find  $rootpath -maxdepth 5 -path "*cache/_source-cache" -not -path "${currpath}*")
+for p in $(find  $rootpath -maxdepth 9 -path "*cache/_source-cache" -not -path "${currpath}*")
 do
   spack mirror add --scope site $(realpath --relative-to=$rootpath $(dirname $(dirname $p))) file://$(dirname $p)
 done
