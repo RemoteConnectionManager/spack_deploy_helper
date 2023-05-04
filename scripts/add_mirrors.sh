dff779a28058bbaf904f12e171ccf9da0f819d0c
Luigi Calori <l.calori@cineca.it>
2023-05-04 20:02:08 +0200

    make command outside env able to run the same when executed when an env is active by adding --no-env flag

--- scripts/add_mirrors.sh
+++ scripts/add_mirrors.sh
@@ -4,5 +4,5 @@ rootpath=$(realpath $currpath/../..)
  
 for p in $(find  $rootpath -maxdepth 9 -path "*cache/_source-cache" -not -path "${currpath}*")
 do
-  spack mirror add --scope site $(realpath --relative-to=$rootpath $(dirname $(dirname $p))) file://$(dirname $p)
+  spack --no-env  mirror add --scope site $(realpath --relative-to=$rootpath $(dirname $(dirname $p))) file://$(dirname $p)
 done
